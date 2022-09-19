c39066cf1d5742677df32f3b2e8e974f27bd4dab
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-09-19 22:26:49 +0300

    Add function to check TouchID support for passwords

--- TweekChecker/AppDelegate.swift
+++ TweekChecker/AppDelegate.swift
@@ -18,7 +18,58 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         if let statusButton = statusItem.button {
             statusButton.image = NSImage(systemSymbolName: "pawprint.circle", accessibilityDescription: "Paw")
         }
+        
+        setupMenus()
     }
+    
+    func setupMenus() {
+            // 1
+            let menu = NSMenu()
+
+            // 2
+            let one = NSMenuItem(title: "Check TouchID", action: #selector(didTapOne) , keyEquivalent: "1")
+            menu.addItem(one)
+
+            menu.addItem(NSMenuItem.separator())
+
+            menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+
+            // 3
+            statusItem.menu = menu
+        }
+    
+        private func changeStatusBarButton(number: Int) {
+            if let button = statusItem.button {
+                button.image = NSImage(systemSymbolName: "\(number).circle", accessibilityDescription: number.description)
+            }
+        }
+
+        @objc func didTapOne() {
+            changeStatusBarButton(number: 1)
+            if isPasswordBtTouchIDExists() {
+                print("YES")
+            } else {
+                print("NO")
+            }
+        }
+    
+    func isPasswordBtTouchIDExists() -> Bool {
+        do {
+            let contents = try String(contentsOfFile: "/etc/pam.d/sudo")
+            let lines = contents.split(separator:"\n")
+            for line in lines {
+                if line.contains("pam_tid.so") {
+                    return true
+                }
+            }
+            
+            return false
+        } catch {
+            print("Error reading PAM file: \(error)")
+            return false
+        }
+    }
+    
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
