d69b6a041734ae514ce257a2cd4a1612a6eab0ec
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-09-19 21:37:57 +0300

    Initial Commit

---
+++ TweekChecker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TweekChecker
+//
+//  Created by Sabyrzhan Tynybayev on 19.09.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
